1be8ba7f63058a72af18e012d4d38e18b2b0abaf
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-08-02 10:53:38 +0200

    Add operator-function to take operator and run a function

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b);
 }
+
+function operator(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Invalid';
+    }
+}
+
+let a;
+let operator;
+let b;
+
