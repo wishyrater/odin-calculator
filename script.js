538e7566f22033fdf467af5d583fdb1c1bf3740f
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-08-06 00:19:10 +0200

    Add calculator logic

--- script.js
+++ script.js
@@ -29,22 +29,46 @@ function calculatorFunction(operator, a, b) {
     }
 }
 
-let a;
-let b;
+let a = 0;
+let b = 0;
 let operator;
 
 const clearButton = document.querySelector("#ac");
 const display = document.querySelector(".display");
+const operatorButtons = document.querySelectorAll("button.operator");
+const numberButtons = document.querySelectorAll("button.number");
+const equalsButton = document.querySelector("button.equals");
 
+// remove all existing text and variable values when clearing the calculator
 clearButton.addEventListener("click", () => {
     a = 0;
     b = 0;
+    operator = undefined;
     display.textContent = "0";
+    operatorButtons.forEach((button) => button.classList.remove("selected"));
 })
 
-const numberButtons = document.querySelectorAll("button.number");
-
+// store values when user clicks number buttons
 numberButtons.forEach((button) => button.addEventListener("click", (e) => {
-    a += e.target.value;
-    display.textContent = +a;
+    if (operator) {
+        b += e.target.value;
+        display.textContent = +b;
+    } else {
+        a += e.target.value;
+        display.textContent = +a;
+    }
 }))
+
+// store operator when user clicks operator button
+operatorButtons.forEach((button) => button.addEventListener("click", (e) => {
+    operator = e.target.value; 
+    e.target.classList.add("selected");
+}))
+
+// display the result when the user clicks equals button
+equalsButton.addEventListener("click", () => {
+    let result = calculatorFunction(operator, +a, +b);
+    display.textContent = parseFloat(result.toFixed(2));
+    operatorButtons.forEach((button) => button.classList.remove("selected"));
+})
+
